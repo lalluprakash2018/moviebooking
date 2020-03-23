f7933b8af1bf9aa00db85898704fac4cce31df06
lalluprakash2018 <35533803+lalluprakash2018@users.noreply.github.com>
2020-03-23 10:59:42 +0530

    Update SignUp.js

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -75,8 +75,7 @@ class SignUp extends Component {
                                     </div>
                                 </div>
                                 <div className="row mt-2">
-                                    <div className="col-3"></div>
-                                    <div className="col-6 text-center">
+                                    <div className="col-12 text-center">
                                         <input type="submit" onClick={this.signin} className="btn btn-success form-control" value="Login" />
                                     </div>
                                 </div>
@@ -94,4 +93,4 @@ class SignUp extends Component {
     }
 }
 
-export default SignUp;
+export default SignUp
