08924c5312a9757c7908ab1f405b78a4c4464c86
lalluprakash2018 <35533803+lalluprakash2018@users.noreply.github.com>
2020-03-23 10:59:16 +0530

    Create DrawGrid.js

---
+++ src/components/DrawGrid.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+// import '../DrawLayout.css';
+
+export class DrawGrid extends Component {
+    onClickSeat(seat) {
+        this.props.onClickData(seat);
+    }
+    render() {
+        return (
+            <div>
+                <div style={{display:'grid', gridTemplateColumns:`repeat(${this.props.left}, 50px)`}} className="seatContainer">
+                    { this.props.seat.map( row =>
+                        <div
+                            className={this.props.reserved.indexOf(row) > -1? 'reserved': 'available'}
+                            key={row} 
+                            onClick = {e => this.onClickSeat(row)}>{row}
+                        </div>) }
+                </div>
+            </div>
+        )
+    }
+}
+
+export default DrawGrid
