1e0907c26e83cedceb7cf9004cb50ec618a6d4ac
lalluprakash2018 <35533803+lalluprakash2018@users.noreply.github.com>
2020-03-23 10:05:47 +0530

    Create SeatLayout.js

---
+++ src/components/SeatLayout.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import TheaterService from '../services/theaters.json';
+
+class SeatLayout extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            theater:{}
+        }
+    }
+    componentDidMount(){
+        TheaterService.map((item)=>
+            (item.name === this.props.match.params.name) ?
+                this.setState({theater:item}) : {}
+        );
+    }
+    render() {
+        return (
+            <div>
+                <h3 className="text-center">{this.state.theater.name}</h3>
+                <div>
+                    <button>Edit Seat layout</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default SeatLayout
