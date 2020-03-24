126142e477f74eff58e54ca6f7ce2a6c57b963cc
lalluprakash2018 <35533803+lalluprakash2018@users.noreply.github.com>
2020-03-24 16:27:33 +0530

    Update Register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -13,10 +13,11 @@ class Register extends Component{
              password:'',
              cfpassword:'',
              dob:'',
+             isValid:false,
              errors:{
-                 fname:'',
-                 email:'',
-                 password:''
+                 fname:false,
+                 email:false,
+                 password:false
              }
         };
         this.handleChange = this.handleChange.bind(this);
@@ -30,17 +31,31 @@ class Register extends Component{
         let errors = this.state.errors;
         switch(name){
             case 'fname':
-                errors.fname=value.length < 5 ? 'Full Name must be 5 characters long!' : '';
+                errors.fname=value.length=== 0 ? 'Please enter the name.' : '';
             break;
             case 'email': 
                 errors.email = validEmailRegex.test(value) ? '' : 'Email is not valid!';
             break;
+            case 'password':
+            case 'cfpassword':
+                errors.password = value.length===0 ? 'Password should not be blank' : '';
+            break;
             default:
             break;
         }
-        this.setState({errors, [name]: value}, ()=> {
-             console.log(errors)
-        })
+        //this.setState({errors, [name]: value}, ()=> {
+            // if(!errors.fname.length || !errors.fname.email || !errors.fname.password){
+            //     this.setState({
+            //         isValid:true
+            //     });
+            // }
+            // else{
+            //     this.setState({
+            //         isValid:false
+            //     });
+            // }
+        //})
+        this.setState({errors, [name]: value});
         this.setState({
             [event.target.name]:event.target.value
         });
@@ -54,12 +69,17 @@ class Register extends Component{
 
     handleSubmit(event) {
         event.preventDefault();
-        const { password, cfpassword } = this.state;
-        if (password !== cfpassword) {
-            alert("Passwords don't match");
-        } else {
-            alert("Done");
-        }
+        // const { password, cfpassword } = this.state;
+        // if (password !== cfpassword) {
+        //     alert("Passwords don't match");
+        // } else {
+        //     alert("Done");
+        // }
+        // const {fname,email,password} = this.state.errors;
+        // console.log(fname);
+        // if(this.state.isValid){
+        //     alert("done");
+        // }
     }
 
     render(){
@@ -74,7 +94,10 @@ class Register extends Component{
                                     <label>Full name:</label>
                                 </div>
                                 <div class="col-6">
-                                    <input type="text" class="form-control" name="fname" value={this.state.fname} onChange={this.handleChange} required/>
+                                    <input type="text" class="form-control" name="fname" value={this.state.fname} onChange={this.handleChange} onBlur={this.handleChange} required/>
+                                </div>
+                                <div style={{display:this.state.fname.length ?'none':'inline'}}>
+                                   {this.state.errors.fname}
                                 </div>
                             </div>
                             <div class="row mt-2">
@@ -111,7 +134,7 @@ class Register extends Component{
                                     <label>Password:</label>
                                 </div>
                                 <div class="col-6">
-                                    <input type="password" class="form-control" name="password" value={this.state.password} onChange={this.handleChange}/>
+                                    <input type="password" class="form-control" name="password" value={this.state.password} onChange={this.handleChange} onBlur={this.handleChange}/>
                                 </div>
                             </div>
                             <div class="row mt-2">
@@ -119,9 +142,10 @@ class Register extends Component{
                                     <label>Conform Password:</label>
                                 </div>
                                 <div class="col-6">
-                                    <input type="password" class="form-control" name="cfpassword" value={this.state.cfpassword} onChange={this.handleChange}/>
+                                    <input type="password" class="form-control" name="cfpassword" value={this.state.cfpassword} onChange={this.handleChange} onBlur={this.handleChange}/>
                                 </div>
                             </div>
+                            <div>{this.state.errors.password}</div>
                             <div class="row mt-2">
                                 <div class="col-12 text-center">
                                     <input type="submit" name="submit" value="Login" class="btn btn-success"/>
